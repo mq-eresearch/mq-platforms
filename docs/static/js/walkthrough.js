d5f97b3a62324a635e0d46faa75f1960f0b4990f
GERRY DEVINE <gerry.devine@mq.edu.au>
2021-08-04 07:01:29 +1000

    Added reset button to final page

--- docs/static/js/walkthrough.js
+++ docs/static/js/walkthrough.js
@@ -6,38 +6,67 @@ function getPageDetails(pageID) {
   }
 }
 
-function activateNextButton(nextPageID, nextPageType) {
+function showNextButton(nextPageID, nextPageType) {
   // Grab next button element
   const nextBtn = document.getElementById("nextFormBtn");
   if (nextPageType == "form") {
-    nextBtn.innerText = "Next >>>";
+    nextBtn.innerHTML = "Next <i class='bi-arrow-right-circle'></i>";
     nextBtn.classList.remove("btn-success");
     nextBtn.classList.add("btn-info");
   } else {
-    nextBtn.innerText = "Finish >>>";
+    nextBtn.innerHTML = "Finish <i class='bi-arrow-right-circle'></i>";
     nextBtn.classList.remove("btn-info");
     nextBtn.classList.add("btn-success");
   }
   nextBtn.setAttribute("onclick", "nextPage(" + nextPageID + ")");
-  nextBtn.classList.remove("invisible");
+  nextBtn.removeAttribute("disabled");
+}
+
+function disableNextButton() {
+  // Grab next button element
+  const nextBtn = document.getElementById("nextFormBtn");
+  nextBtn.setAttribute("disabled", "");
 }
 
 function hideNextButton() {
   // Grab next button element
   const nextBtn = document.getElementById("nextFormBtn");
-  nextBtn.classList.add("invisible");
+  nextBtn.classList.add("d-none");
+}
+
+function showBackButton(previousPageID) {
+  const backBtn = document.getElementById("backBtn");
+  backBtn.setAttribute("onclick", `nextPage(${previousPageID})`);
+  backBtn.classList.remove("d-none");
+}
+
+function hideBackButton() {
+  const backBtn = document.getElementById("backBtn");
+  backBtn.removeAttribute("onclick");
+  backBtn.classList.add("d-none");
+}
+
+function showResetButton() {
+  const backBtn = document.getElementById("resetBtn");
+  // backBtn.setAttribute("onclick", `nextPage(${previousPageID})`);
+  backBtn.innerHTML = 'Reset <i class="bi-arrow-repeat"></i>';
+  backBtn.classList.remove("d-none");
+}
+
+function hideResetButton() {
+  const backBtn = document.getElementById("resetBtn");
+  backBtn.classList.add("d-none");
 }
 
 function setAnswers(answers) {
   var html = [];
   answers.forEach(function (answer) {
-    myanswer = `onclick="activateNextButton(${answer["next_page_id"]}, ${answer["next_page_type"]})"`;
     var answer_html = [
       '<div class="form-check mb-2">',
       '<input class="form-check-input" type="radio"',
       'name="purposeRadio"',
       'id="' + answer["answer_code"] + '"',
-      `onclick="activateNextButton(${answer["next_page_id"]}, '${answer["next_page_type"]}')"`,
+      `onclick="showNextButton(${answer["next_page_id"]}, '${answer["next_page_type"]}')"`,
       'value="' + answer["answer_code"] + '"',
       "/>",
       '<label class="form-check-label" for="' + answer["answer_code"] + '">',
@@ -53,7 +82,6 @@ function setAnswers(answers) {
 
 function setResults(results) {
   var html = [];
-  console.log(results);
   results.forEach(function (result) {
     var answer_html = ['<div class="mb-2">', `<p>${result}</p>`, "</div>"].join(
       "\n"
@@ -63,7 +91,7 @@ function setResults(results) {
     //   '<input class="form-check-input" type="radio"',
     //   'name="purposeRadio"',
     //   'id="' + answer["answer_code"] + '"',
-    //   `onclick="activateNextButton(${answer["next_page_id"]}, '${answer["next_page_type"]}')"`,
+    //   `onclick="showNextButton(${answer["next_page_id"]}, '${answer["next_page_type"]}')"`,
     //   'value="' + answer["answer_code"] + '"',
     //   "/>",
     //   '<label class="form-check-label" for="' + answer["answer_code"] + '">',
@@ -78,12 +106,14 @@ function setResults(results) {
 }
 
 function nextPage(pageID) {
-  // Start by hiding both form and result sections and the next button
+  // Start by hiding both form and result sections, disabling the next button, and hiding the back button and reset button
   const formBody = document.getElementById("form-body");
   formBody.style.display = "none";
   const resultBody = document.getElementById("result-body");
   resultBody.style.display = "none";
-  hideNextButton();
+  hideBackButton();
+  hideResetButton();
+  disableNextButton();
 
   const nextPage = getPageDetails(pageID);
   if (nextPage["page_type"] == "form") {
@@ -108,9 +138,19 @@ function populateForm(page) {
   answers = setAnswers(currentForm["answers"]);
   formAnswers.innerHTML = answers;
 
-  // Finally, show the form section
+  // Show the form section
   const formBody = document.getElementById("form-body");
   formBody.style.display = "block";
+
+  // // Finally populate and show the back button if applicable
+
+  // // Shohideorm section
+  // const formBody = document.getElementById("form-body");
+  // formBody.style.display = "block";
+  // Finally populate and show the back button if applicable
+  if (currentForm["previous_page_id"]) {
+    showBackButton(currentForm["previous_page_id"]);
+  }
 }
 
 function populateResult(page) {
@@ -136,6 +176,13 @@ function populateResult(page) {
   // Finally, show the results section
   const resultBody = document.getElementById("result-body");
   resultBody.style.display = "block";
+
+  // Finally handle the next, back and reset buttons if applicable
+  hideNextButton();
+  showResetButton();
+  // if (currentPage["previous_page_id"]) {
+  showBackButton(currentPage["previous_page_id"]);
+  // }
 }
 
 fetch("forms.json")
